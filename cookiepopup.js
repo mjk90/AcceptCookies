cc9b7d07cc60bbf80c1dcbfd3c30ee5e7ca36e20
Matthew Kelly <mkelly2@live.co.uk>
2017-09-12 15:49:53 +0100

    Changed main funtion to be a jQuery plugin method

--- cookiepopup.js
+++ cookiepopup.js
@@ -1,61 +1,69 @@
-const cookiePopupHtml = '<div id="cookie-popup-container">' +
-    '<div class="cookie-popup" style="display: none;">' +
-        '<div class="cookie-popup-inner">' +
-            '<div class="cookie-popup-left">' +
-                '<div class="cookie-popup-headline">This website uses cookies</div>' +
-                '<div class="cookie-popup-sub-headline">By using this site, you agree to our use of cookies.</div>' +
-            '</div>' +
+(function ( $ ) {
+    "use strict";
 
-            '<div class="cookie-popup-right">' +
-                '<a href="#" class="cookie-popup-accept-cookies">Accept</a>' +
-                '<a href="#" class="cookie-popup-learn-more">Learn More</a>' +
-            '</div>' +
+    const cookiePopupHtml = '<div id="cookie-popup-container">' +
+        '<div class="cookie-popup" style="display: none;">' +
+            '<div class="cookie-popup-inner">' +
+                '<div class="cookie-popup-left">' +
+                    '<div class="cookie-popup-headline">This website uses cookies</div>' +
+                    '<div class="cookie-popup-sub-headline">By using this site, you agree to our use of cookies.</div>' +
+                '</div>' +
 
+                '<div class="cookie-popup-right">' +
+                    '<a href="#" class="cookie-popup-accept-cookies">Accept</a>' +
+                    '<a href="#" class="cookie-popup-learn-more">Learn More</a>' +
+                '</div>' +
+            '</div>' +
+            '<div class="cookie-popup-lower" style="display: none;">' +
+                'Cookies are small text files held on your computer. They allow us to give you the best browsing experience possible and mean we can understand how you use our site.' +
+            '</div>' +
         '</div>' +
-    '</div>' +
-'</div>';
+    '</div>';
 
-var cookiePopup = function(options) {
-    var cookiesAccepted = getCookie("cookiesAccepted");
+    $.extend({
+        acceptCookies : function(options) {
+            var cookiesAccepted = getCookie("cookiesAccepted");
 
-    if (!cookiesAccepted) {
-        var cookiePopup = $(cookiePopupHtml);
-        var position = "bottom";
+            if (!cookiesAccepted) {
+                var cookiePopup = $(cookiePopupHtml);
+                var position = "bottom";
 
-        if(options != undefined) {
-            position = options.position != undefined ? options.position : "bottom";
+                if(options != undefined) {
+                    position = options.position != undefined ? options.position : "bottom";
 
-            if(options.title != undefined)
-                cookiePopup.find('.cookie-popup-headline').text(options.title);
-            if(options.text != undefined)
-                cookiePopup.find('.cookie-popup-sub-headline').text(options.text);
-            if(options.theme != undefined)
-                cookiePopup.addClass("theme-" + options.theme);
+                    if(options.title != undefined)
+                        cookiePopup.find('.cookie-popup-headline').text(options.title);
+                    if(options.text != undefined)
+                        cookiePopup.find('.cookie-popup-sub-headline').text(options.text);
+                    if(options.theme != undefined)
+                        cookiePopup.addClass("theme-" + options.theme);
 
-            if(options.learnMore != undefined) {
-                if(options.learnMore == false)
-                    cookiePopup.find(".cookie-popup-learn-more").remove();
-            }
+                    if(options.learnMore != undefined) {
+                        if(options.learnMore == false)
+                            cookiePopup.find(".cookie-popup-learn-more").remove();
+                    }
 
-            if(options.customText != undefined) {
-                if(options.customText.headline != undefined)
-                    cookiePopup.find(".cookie-popup-headline").text(options.customText.headline);
-                if(options.customText.subHeadline != undefined)
-                    cookiePopup.find(".cookie-popup-sub-headline").text(options.customText.subHeadline);
-                if(options.customText.acceptButton != undefined)
-                    cookiePopup.find(".cookie-popup-accept-cookies").text(options.customText.acceptButton);
-                if(options.customText.learnMoreButton != undefined)
-                    cookiePopup.find(".cookie-popup-learn-more").text(options.customText.learnMoreButton);
-            }
+                    if(options.customText != undefined) {
+                        if(options.customText.headline != undefined)
+                            cookiePopup.find(".cookie-popup-headline").text(options.customText.headline);
+                        if(options.customText.subHeadline != undefined)
+                            cookiePopup.find(".cookie-popup-sub-headline").text(options.customText.subHeadline);
+                        if(options.customText.acceptButton != undefined)
+                            cookiePopup.find(".cookie-popup-accept-cookies").text(options.customText.acceptButton);
+                        if(options.customText.learnMoreButton != undefined)
+                            cookiePopup.find(".cookie-popup-learn-more").text(options.customText.learnMoreButton);
+                        if(options.customText.learnMoreInfo != undefined)
+                            cookiePopup.find(".cookie-popup-lower").text(options.customText.learnMoreInfo);
+                    }
+                }
+                
+                cookiePopup.find('.cookie-popup').addClass("position-" + position);
+                $('body').append(cookiePopup);
+                $('.cookie-popup').slideToggle();
+            }    
         }
-        
-        cookiePopup.find('.cookie-popup').addClass("position-" + position);
-        $('body').append(cookiePopup);
-        $('.cookie-popup').slideToggle();
-    }    
-}
+    });
 
-$(function () {
     $(document).on('click', '.cookie-popup-accept-cookies', function(e) {
         //var expiryDate = moment($("#acceptCookiesExpiryDateUtc").val(), "DD/MM/YYYY hh:mm");
         //var expires = "expires=" + expiryDate.toDate().toUTCString();
@@ -63,21 +71,25 @@ $(function () {
         var expires = "expires=08/11/2017"
         document.cookie = "cookiesAccepted=true; " + expires + "; path=/";
         $('.cookie-popup').slideToggle();
+    }).on('click', '.cookie-popup-learn-more', function(e) {
+        e.preventDefault();
+        $('.cookie-popup-lower').slideToggle();
     });
-});
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+        return "";
     }
-    return "";
-}
+ 
+}( jQuery ));
